92d9fc73a8bc79bbe8b82f329227fc9fad00c2a3
Fan Yang <fanyang.phy@gmail.com>
2019-03-02 21:14:26 -0800

    Finished lab1.

--- labs/lab1-blink/part3/blink.c
+++ labs/lab1-blink/part3/blink.c
@@ -12,22 +12,28 @@
  * - initialize a structure with all the fields.
  */
 
+#include "rpi.h"
+
 // see broadcomm documents for magic addresses.
-#define GPIO_BASE 0x20200000
-volatile unsigned *gpio_fsel1 = (volatile unsigned *)(GPIO_BASE + 0x04);
+#define PERIPHERAL_BASE       0x3F000000UL
+#define GPIO_BASE       ( PERIPHERAL_BASE + 0x200000UL )
+volatile unsigned *gpio_fsel0 = (volatile unsigned *)(GPIO_BASE + 0x00);
 volatile unsigned *gpio_set0  = (volatile unsigned *)(GPIO_BASE + 0x1C);
 volatile unsigned *gpio_clr0  = (volatile unsigned *)(GPIO_BASE + 0x28);
 
 // XXX might need memory barriers.
 void gpio_set_output(unsigned pin) {
 	// gpio_fsel1  --- set 'pin' to output.
+    gpio_fsel0[pin/10] = (gpio_fsel0[pin/10] & ~(0b111 << 3*(pin%10))) | (0b001 << 3*(pin%10));
 }
 
 void gpio_set_on(unsigned pin) {
 	// use gpio_set0
+    gpio_set0[pin/32] |= 1 << pin%32;
 }
 void gpio_set_off(unsigned pin) {
 	// use gpio_clr0
+    gpio_clr0[pin/32] |= 1 << pin%32;
 }
 
 // countdown 'ticks' cycles; the asm probably isn't necessary.
@@ -36,15 +42,27 @@ void delay(unsigned ticks) {
 		asm("add r1, r1, #0");
 }
 
-int notmain ( void ) {
-	int led = 20;
+// define: dummy to immediately return and PUT32 as above.
+ void reboot(void) {
+      const int PM_RSTC = PERIPHERAL_BASE + 0x0010001c;
+      const int PM_WDOG = PERIPHERAL_BASE + 0x00100024;
+      const int PM_PASSWORD = 0x5a000000;
+      const int PM_RSTC_WRCFG_FULL_RESET = 0x00000020;
+      put32((volatile int *)PM_WDOG, PM_PASSWORD | 1);
+      put32((volatile int *)PM_RSTC, PM_PASSWORD | PM_RSTC_WRCFG_FULL_RESET);
+ }
 
-  	gpio_set_output(led);
-        while(1) {
-                gpio_set_on(led);
-                delay(1000000);
-                gpio_set_off(led);
-                delay(1000000);
-        }
-	return 0;
+
+int notmain ( void ) {
+    int led = 20;
+
+    gpio_set_output(led);
+    for (int i=0; i<100; i++) {
+            gpio_set_on(led);
+            delay(1000000);
+            gpio_set_off(led);
+            delay(1000000);
+    }
+    reboot();
+    return 0;
 }
--- labs/lab1-blink/part3/rpi.h
+++ labs/lab1-blink/part3/rpi.h
@@ -7,4 +7,16 @@ unsigned (get32)(volatile void *addr);
 // *(volatile unsigned *)addr = v
 void (put32)(volatile void *addr, unsigned val);
 
+// return *(volatile unsigned *)addr
+unsigned short (get16)(volatile void *addr);
+
+// *(volatile unsigned *)addr = v
+void (put16)(volatile void *addr, unsigned short val);
+
+// return *(volatile unsigned *)addr
+unsigned char (get8)(volatile void *addr);
+
+// *(volatile unsigned *)addr = v
+void (put8)(volatile void *addr, unsigned char val);
+
 #endif /* __RPI_H__ */
