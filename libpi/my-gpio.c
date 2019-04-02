ae6a340ed97da1288fb83b7090f5aa879b7da3f5
Fan Yang <fanyang.phy@gmail.com>
2019-04-02 10:39:55 -0700

    Replaced some default cs140e library code with my own.

--- libpi/my-gpio.c
+++ libpi/my-gpio.c
@@ -1,59 +1,87 @@
-#include "rpi.h"
+/* Fan Yang's implementation of the gpio functions.
+ */
 #include "gpio.h"
 
-#define GPIO_BASE 0x20200000
-#define GPSET0  (GPIO_BASE+0x1c)
-#define GPCLR0  (GPIO_BASE+0x28)
-#define GPLEV0  (GPIO_BASE+0x34)
+#define GPIO_PIN_FIRST 0
+#define GPIO_PIN_LAST 53
+#define GPIO_INVALID_REQUEST  -1  // return value for invalid request
 
-int gpio_set_function(unsigned pin, unsigned val) {
-        if(pin >= 32)
-                return -1;
-	if((val & 0b111) != val)
-		return -1;
+// see broadcomm documents for magic addresses.
+#define PERIPHERAL_BASE       0x3F000000UL
+#define GPIO_BASE       ( PERIPHERAL_BASE + 0x200000UL )
+volatile unsigned *gpio_fsel0 = (volatile unsigned *)(GPIO_BASE + 0x00);
+volatile unsigned *gpio_set0  = (volatile unsigned *)(GPIO_BASE + 0x1C);
+volatile unsigned *gpio_clr0  = (volatile unsigned *)(GPIO_BASE + 0x28);
+volatile unsigned *gpio_lev0  = (volatile unsigned *)(GPIO_BASE + 0x34);
 
-        unsigned *gp = ((unsigned *)GPIO_BASE + pin/10);
-        unsigned off = (pin%10)*3;
-
-        unsigned v = get32(gp);
-                v &= ~(0b111 << off);
-                v |= (val << off);
-        put32(gp, v);
-        return 0;
+void gpio_init(void) {
+    // do nothing
 }
 
-int gpio_set_output(unsigned pin) {
-	return gpio_set_function(pin, GPIO_FUNC_OUTPUT);
+int gpio_set_function(unsigned int pin, unsigned int function) {
+    // see page 91 of BCM2835 documentation
+    // may need to block. will fix later.
+    if (pin < GPIO_PIN_FIRST || pin > GPIO_PIN_LAST)
+        return -1;
+    gpio_fsel0[pin/10] = (gpio_fsel0[pin/10] & ~(0b111 << 3*(pin%10))) | (function << 3*(pin%10));
+    return 0;
 }
 
-int gpio_set_input(unsigned pin) {
-	return gpio_set_function(pin, GPIO_FUNC_INPUT);
+unsigned int gpio_get_function(unsigned int pin) {
+    // see page 91 of BCM2835 documentation
+    if (pin < GPIO_PIN_FIRST || pin > GPIO_PIN_LAST)
+        return GPIO_INVALID_REQUEST;
+    return (gpio_fsel0[pin/10] >> 3*(pin%10)) & 0b111;
+}
+
+int gpio_set_input(unsigned int pin) {
+    return gpio_set_function(pin, GPIO_FUNC_INPUT);
+}
+
+int gpio_set_output(unsigned int pin) {
+    // see page 91 of BCM2835 documentation
+    return gpio_set_function(pin, GPIO_FUNC_OUTPUT);
+}
+
+int gpio_write(unsigned int pin, unsigned int val) {
+    if (pin < GPIO_PIN_FIRST || pin > GPIO_PIN_LAST)
+        return -1;
+    switch (val) {
+        case 0:
+            gpio_clr0[pin/32] |= 1 << pin%32;
+            break;
+        case 1:
+            gpio_set0[pin/32] |= 1 << pin%32;
+            break;
+        default:
+            // do nothing
+            return -1;
+    }
+    return 0;
 }
 
 int gpio_set_off(unsigned pin) {
-	if(pin >= 32)
-		return -1;
-	PUT32(GPCLR0, 1 << pin);
-	return 0;
+    return gpio_write(pin, 0);
 }
+
 int gpio_set_on(unsigned pin) {
-	if(pin >= 32)
-		return -1;
-	PUT32(GPSET0, 1 << pin);
-	return 0;
+    return gpio_write(pin, 1);
 }
 
-int gpio_write(unsigned pin, unsigned v) {
-	if(v)
-		return gpio_set_on(pin);
-	else
-		return gpio_set_off(pin);
-}
-
-// doesn't do error checking...
 unsigned gpio_read(unsigned pin) {
-	assert(pin < 32);
-        unsigned bank  = (GPLEV0 + pin/32);
-        unsigned off = (pin%32);
-	return (GET32(bank) >> off) & 1;
+    if (pin < GPIO_PIN_FIRST || pin > GPIO_PIN_LAST)
+        return GPIO_INVALID_REQUEST;
+    return (gpio_lev0[pin/32] >> (pin%32)) & 0b1;
+}
+
+int gpio_set_pud(unsigned pin, unsigned pud) {
+    // not implemented
+}
+
+int gpio_set_pullup(unsigned pin) {
+    // not implemented
+}
+
+int gpio_set_pulldown(unsigned pin) {
+    // not implemented
 }
